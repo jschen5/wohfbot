1833aff116db56e86b4afde82236206e8d3036db
Jason Chen <jasonschen5@gmail.com>
2019-12-24 15:56:00 -0800

    some sh** that kinda works for pulling channel messages for a user

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ const discordConfig = require('../discord.json');
 
 const client = new Discord.Client();
 
+function isGeneralChannel(channel: Discord.Channel): boolean {
+    return (channel as Discord.TextChannel).name === 'general';
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -12,13 +16,30 @@ client.on('message', async msg => {
         msg.reply('wohf pong');
     }
 
-    if (msg.content === 'fetch channels') {
-        const msgs = new Discord.Collection<string, Discord.Message>();
+    if (msg.content.match(/^fetch channels/)) {
+        const targetUser = msg.mentions.users.first();
+        let allMessages = new Discord.Collection<string, Discord.Message>();
 
-        // const msgs = await msg.channel.fetchMessages({
-        //     limit: 100
-        // });
-        console.log(msgs);
+        const channel = client.channels.find(c =>
+            c.type === 'text' && isGeneralChannel(c)) as Discord.TextChannel;
+
+        let lastOldestMessageId: string | null = null;
+
+        while (allMessages.size < 500) {
+            const msgs = await channel.fetchMessages({
+                limit: 100,
+                ...lastOldestMessageId && { before: lastOldestMessageId }
+            });
+            const msgsForUser = msgs.filter(m => m.author.id === targetUser.id);
+            allMessages = allMessages.concat(msgsForUser);
+            lastOldestMessageId = msgs.last().id;
+        }
+
+        const msgArray = allMessages
+            .filter(m => m.cleanContent.length > 0)
+            .map(m => m.cleanContent);
+
+        console.log(msgArray);
     }
 });
 
