38ecc7246c4b21e0a3073cfe668b2939d91d0213
Jason Chen <jasonschen5@gmail.com>
2019-12-24 14:28:20 -0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import * as Discord from 'discord.js';
+const discordConfig = require('../discord.json');
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+client.on('message', async msg => {
+    if (msg.content === 'wohf ping') {
+        msg.reply('wohf pong');
+    }
+
+    if (msg.content === 'fetch channels') {
+        const msgs = new Discord.Collection<string, Discord.Message>();
+
+        // const msgs = await msg.channel.fetchMessages({
+        //     limit: 100
+        // });
+        console.log(msgs);
+    }
 });
 
-client.login('NjU5MTIxOTgxOTI4NzY3NDg5.XgJy2Q.j8b2vOncMBxdWKeHmtCIiTEtCHc');
+client.login(discordConfig.token);
