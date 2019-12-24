f87b6497633519aaffedbdcafcbd87098b326e79
Andrew Chen <aychen92@gmail.com>
2019-12-24 15:34:15 -0800

    Added message generation using markov model.

--- src/markov.ts
+++ src/markov.ts
@@ -2,6 +2,8 @@ interface WordFreqs {
     [token: string]: number,
 }
 
+type WordCounts = WordFreqs;
+
 interface TokenToFollowingTokens {
     [token: string]: string[]
 }
@@ -10,12 +12,22 @@ interface MarkovChain {
     [token: string]: WordFreqs
 }
 
+interface MarkovData {
+    markovChain: MarkovChain,
+    startWords: string[],
+    endWords: string[],
+}
 
-function markovChainFromMessages(messages: string[]): MarkovChain {
+function markovDataFromMessages(messages: string[]): MarkovData {
     const tokenToNextTokens = {};
+    const startWords = [];
+    const endWords = [];
 
     for (const message of messages) {
         const corpus = message.split(' ');
+
+        startWords.push(corpus[0]);
+        endWords.push(corpus[corpus.length - 1]);
 
         corpus.forEach((token: string, index: number) => {
             const nextToken = corpus[index + 1];
@@ -33,7 +45,7 @@ function markovChainFromMessages(messages: string[]): MarkovChain {
         .reduce((chain: MarkovChain, token: string) => {
             const nextTokens = tokenToNextTokens[token];
             const wordCounts = nextTokens
-                .reduce((acc: WordFreqs, currVal: string): WordFreqs => {
+                .reduce((acc: WordCounts, currVal: string): WordCounts => {
                     if (acc.hasOwnProperty(currVal)) {
                         acc[currVal]++;
                     } else {
@@ -57,5 +69,45 @@ function markovChainFromMessages(messages: string[]): MarkovChain {
             };
         }, {});
 
-    return markovChain;
+    return { markovChain, startWords, endWords };
+}
+
+function messageFromMarkovData(markovData: MarkovData): string {
+    const { markovChain, startWords, endWords } = markovData;
+
+    // First choose a random start word.
+    const startWord = startWords[Math.floor(Math.random() * startWords.length)];
+
+    let message = [startWord];
+    let currWord = startWord;
+    let nextWord;
+    while (!endWords.hasOwnProperty(nextWord)) {
+        nextWord = chooseNextWord(currWord, markovChain);
+        
+        if (!nextWord) break;
+
+        message = [...message, nextWord];
+
+        currWord = nextWord;
+    }
+
+    return message.join(' ');
+}
+
+function chooseNextWord(currentWord: string, markovChain: MarkovChain): string {
+    const threshold = Math.random();
+    let probTotal = 0;
+    
+    const wordFreqs = markovChain[currentWord];
+    if (!wordFreqs) {
+        return '';
+    }
+
+    for (const word of Object.keys(wordFreqs)) {
+        probTotal += wordFreqs[word];
+
+        if (probTotal > threshold) return word;
+    }
+
+    return '';
 }
